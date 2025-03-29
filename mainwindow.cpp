e25221496fc251299218909e355e033ddc915fe7
Jack <jack@ggmail.com>
2025-03-29 15:36:11 +0800

    lalala

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "dwgobject.h"
+#include "dwgpoint.h"
+#include <QMouseEvent>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +14,36 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionPoint_triggered()
+{
+    curDrawType = DPoint;           // 点击绘图菜单中的Point菜单项，设置当前绘图类型为点
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    m_mainlist.clear();             // 点New，就是清除主链表，并刷新屏幕
+}
+
+
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    DwgPoint *pt;
+    if(curDrawType & DPoint)
+    {
+        pt = new DwgPoint;
+        *pt = event -> pos();
+        m_mainlist.append(pt);
+    }
+    update();
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    for(auto& i : m_mainlist)
+        i->Draw(&painter);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "dwgobject.h"
+
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +19,19 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QList<DwgObject *>  m_mainlist;     // 定义了一个数组用于保存所有屏幕上绘制的图元对象
+    DrawType curDrawType = None;       // 定义一个变量用来记录当前鼠标正在绘制哪种图元
+private slots:
+    void on_actionPoint_triggered();
+
+    void on_actionNew_triggered();
 
 private:
     Ui::MainWindow *ui;
+
+    // QWidget interface
+protected:
+    virtual void mousePressEvent(QMouseEvent *event);
+    virtual void paintEvent(QPaintEvent *event);
 };
 #endif // MAINWINDOW_H
